08009fc3817779df2e2383ef8d60db55bed8d20c
Razmo <you@example.com>
2023-10-06 18:10:00 +0200

    exo2

---
+++ ex_02/Gecko.php
@@ -0,0 +1,21 @@
+<?php
+class Gecko {
+    public $name;
+    
+    public function __construct($name = null) 
+    { 
+        if($name!==null)
+        
+        {
+            $this -> name = $name;
+            echo "Hello $name ! \n";
+        }
+        
+         else {echo 'Hello !' . PHP_EOL;}
+    }
+
+}
+
+new Gecko();
+
+new Gecko("toto");
